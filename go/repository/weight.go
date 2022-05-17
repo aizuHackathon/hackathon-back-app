a54a1dfd8155c86093c74f6c9bb69f21680c63ab
hond0413 <hondsoccer0413@gmail.com>
2022-05-17 13:01:53 +0900

    fix: data order

--- go/repository/weight.go
+++ go/repository/weight.go
@@ -27,7 +27,10 @@ func NewWeight() IWeight {
 
 func (r *Weight) ByUserID(id int64) (*model.Weights, error) {
 	m := &model.Weights{}
-	_, err := r.session.Select("*").From("weights").Where("user_id = ?", id).Load(m)
+	_, err := r.session.Select("*").From("weights").
+		Where("user_id = ?", id).
+		OrderAsc("created_at").
+		Load(m)
 	if err != nil {
 		return nil, fmt.Errorf("fetch error :%v", err)
 	}
