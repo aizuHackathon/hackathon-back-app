d462d612c2d484d9324ffd2c58fb51fbb19c421c
Shunsuke0425 <migihawkssince2000@gmail.com>
2022-05-24 17:12:02 +0900

    update function about random

--- go/service/mongon.go
+++ go/service/mongon.go
@@ -1,15 +1,8 @@
 package service
 
 import (
-	"app/db"
 	"app/model"
 	"app/repository"
-
-	"math/rand"
-	"time"
-	"fmt"
-
-	"github.com/gocraft/dbr"
 )
 
 type (
@@ -19,32 +12,19 @@ type (
 
 	Mongon struct {
 		repository repository.IMongon
-		session *dbr.Session
 	}
 )
 
 func NewMongon() IMongon {
 	return &Mongon{
 		repository: repository.NewMongon(),
-		session: db.GetSession(),
 	}
 }
 
 func (s *Mongon) GetMongon() (*model.Mongon, error) {
-	l := &model.Mongon{}
-	_, err := s.session.Select("id").From("Mongons").Load(l)
+	Id, err := s.repository.RandomId()
 
-	idsLength := &model.Count{}
-	d, err := s.session.Select("count(*) as value").From("Mongons").Load(idsLength)
-
-	if err != nil {
-		return nil, fmt.Errorf("fetch error :%v", d)
-	}
-
-	rand.Seed(time.Now().UnixNano())
-	randId := rand.Int63n(idsLength.Value) + 1
-
-	m, err := s.repository.MongonByRandom(randId)
+	m, err := s.repository.MongonByRandom(Id)
 	if err != nil {
 		return nil, err
 	}
